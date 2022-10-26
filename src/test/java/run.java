3bba9b87d88ebd93131ab7877f278ff6c544ce6f
“aimimiplpl” <379015919@qq.com>
2022-10-26 22:59:07 +0800

    reset 888

--- src/test/java/run.java
+++ src/test/java/run.java
@@ -1,6 +1,5 @@
 public class run {
     public static void main(String[] args) {
-        System.out.println(777777);
-        System.out.println(8888+1);
+        System.out.println(8888);
     }
 }
